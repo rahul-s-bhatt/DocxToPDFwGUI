b8aba4273446a88fc22276f760bf18ac57bd67b1
Nisrin Dhoondia <38102371+mygoal-javadeveloper@users.noreply.github.com>
2021-09-09 17:50:42 +0530

    Update docxToPDF.py

--- docxToPDF.py
+++ docxToPDF.py
@@ -6,16 +6,17 @@ wdFormatPDF = 17
 infolder="C:\\Users\\BHATT\\Desktop\\ConvertedPDF"
 out_folder ="C:\\Users\\BHATT\\Desktop\\ConvertedPDF"
 
+word = comtypes.client.CreateObject('Word.Application')
 for in_file_name in os.listdir(infolder):
 	print(in_file_name)
-	in_file=infolder+in_file_name
-	word = comtypes.client.CreateObject('Word.Application')
+	in_file = infolder + '\\' + in_file_name
 	doc = word.Documents.Open(in_file)
 	print("\n"+in_file+" opened")
 	
-	outfile_name=in_file_name.replace("docx","pdf")
-	out_file =out_folder+outfile_name
+	outfile_name = in_file_name.replace("docx","pdf")
+	out_file = out_folder + '\\' + outfile_name
 	doc.SaveAs(out_file, FileFormat=wdFormatPDF)
 	doc.Close()
-	word.Quit()
 	print("successfully converted"+outfile_name)
+word.Quit()
+	
